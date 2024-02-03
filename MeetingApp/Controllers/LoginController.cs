9ac2004d5d5ecf2a1c3e6d35d22974cf5412ca9e
ZUOskan <zaferulasoskan@gmail.com>
2024-02-03 21:42:31 +0300

    Add library for User Authentication, work in progress Register complete, will add email confirmation

---
+++ MeetingApp/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
--- MeetingApp/Program.cs
+++ MeetingApp/Program.cs
@@ -1,14 +1,28 @@
 using MeetingApp.BusinessLogic.LogicServices;
 using MeetingApp.DataAccess.DataContext;
 using MeetingApp.DataAccess.DataServices;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MeetingApp.Data;
+using MeetingApp.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MeetingAppContextConnection") ?? throw new InvalidOperationException("Connection string 'MeetingAppContextConnection' not found.");
+
+builder.Services.AddDbContext<MeetingAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MeetingAppContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IMeetingsLogic, MeetingsLogic>();
 builder.Services.AddSingleton<IMeetingsDataAccess, MeetingsDataAccess>();
 builder.Services.AddSingleton<IDapperOrmHelper, DapperOrmHelper>();
+builder.Services.AddRazorPages();
+
+
+
+
 
 var app = builder.Build();
 
@@ -30,5 +44,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
